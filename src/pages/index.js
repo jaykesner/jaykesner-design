c4586874df892c8df378261b395181b64d71af39
Jay Kesner <jaykesner@gmail.com>
2020-06-28 18:16:52 -0400

    setup material ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,72 @@
 import React from "react"
+import {
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  Paper,
+  Typography,
+  Box,
+  Container,
+  Grid,
+} from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles(theme => ({
+  grid: {
+    marginTop: theme.spacing(2),
+  },
+}))
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const styles = useStyles()
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="static" elevation={0}>
+        <Toolbar>
+          <Typography variant="h6">Jay Kesner - Game Design</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm">
+        <Typography variant="h1" align="center">
+          Hello!
+        </Typography>
+        <Typography variant="h5" align="center">
+          I'm a North Carolina based game designer
+        </Typography>
+        <Grid container spacing={2} className={styles.grid}>
+          <Grid item xs={12} sm={4}>
+            <Paper variant="outlined" square>
+              <Typography variant="h5">Content</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Paper variant="outlined" square>
+              <Typography variant="h5">Content</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Paper variant="outlined" square>
+              <Typography variant="h5">Content</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Paper variant="outlined" square>
+              <Typography variant="h5">Content</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Paper variant="outlined" square>
+              <Typography variant="h5">Content</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Paper variant="outlined" square>
+              <Typography variant="h5">Content</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  )
 }
